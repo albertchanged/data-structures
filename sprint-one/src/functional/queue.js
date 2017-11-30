cddcf4f2410664e57fcc8b5350ff9583f5fa2581
albertchanged <albertchanged@gmail.com>
2017-11-29 20:39:31 -0800

    Add final functional/queue.js
    This code satisfies the Queue tests using the Functional
    instantiation pattern.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,31 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var count = 0;  
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[count] = value;
+    count++;
   };
 
   someInstance.dequeue = function() {
+    if (count > 0) {
+      
+      var dequeued = storage[0];
+      delete storage[0];
+  
+      for (var i = 1; i <= count; i++) {
+        var temp = storage[i];
+        storage[i - 1] = temp;
+      }
+      count--;
+      return dequeued;
+    }
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
