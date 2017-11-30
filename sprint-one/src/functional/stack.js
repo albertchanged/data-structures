937884d78a4d942f249e4df257f7d19a0e36feb2
albertchanged <albertchanged@gmail.com>
2017-11-29 16:41:14 -0800

    Add fully working functional/stack.js
    This code satisfies all Functional tests in Stack.

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,27 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var count = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    if (!storage.hasOwnProperty(value)) {
+      storage[count] = value;
+      count++;
+    }
   };
-
+ //{ 1 : 'a', 2 : 'b'}  count = 2 props
   someInstance.pop = function() {
+    if (count > 0) {
+      count--; // storage[1] = a
+      var popped = storage[count]; // 2 : 'b'
+      delete storage[count]; // { 1 : 'a'}
+      return popped;
+    }
   };
 
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
