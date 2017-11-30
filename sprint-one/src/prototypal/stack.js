2dc9aff9a9e349ae4be304c71fa89cf9b64b57ec
albertchanged <albertchanged@gmail.com>
2017-11-29 20:03:48 -0800

    Push final prototypal/stack.js
    This code satisfies all Stack tests using the Prototypal
    instantiation pattern.

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = Object.create(stackMethods);
+
+  stack.count = 0;
+
+  return stack;
 };
 
-var stackMethods = {};
-
-
+var stackMethods = {
+  push: function(value) {
+    this[this.count] = value;
+    this.count++;
+  },
+  pop: function() {
+    if (this.count > 0) {
+      this.count--;
+      var popped = this[this.count];
+      delete this[this.count];
+      return popped;
+    }
+  },
+  size: function() {
+    return this.count;
+  }
+};
