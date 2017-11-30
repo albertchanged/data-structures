08543afa1c1d28374d1a4316f5d789bca33f6bc6
albertchanged <albertchanged@gmail.com>
2017-11-29 20:58:33 -0800

    Push final functional-shared/queue.js
    This code satisfies all Queue tests using the Functional
    Shared instantiation pattern.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,38 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queue = {};
+  queue.count = 0;
+
+  queue.enqueue = queueMethods.enqueue;
+  queue.dequeue = queueMethods.dequeue;
+  queue.size = queueMethods.size;
+
+  return queue;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this[this.count] = value;
+  this.count++;
+};
+
+queueMethods.dequeue = function() {
+  if (this.count > 0) {
+    var dequeued = this[0];
+    delete this[0];
+
+    for (var i = 1; i <= this.count; i++) {
+      var temp = this[i];
+      this[i - 1] = temp;
+    }
+    this.count--;
+    return dequeued;
+  }
+};
+
+queueMethods.size = function() {
+  return this.count;
+};
 
