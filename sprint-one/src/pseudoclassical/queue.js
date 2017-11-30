d12f981fa356cfeba05e166946ff55bc1fa15cab
albertchanged <albertchanged@gmail.com>
2017-11-29 21:00:54 -0800

    Push final pseudoclassical/queue.js
    This code satisfies all Queue tests using the Pseudoclassical
    instantiation pattern.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.count = 0;
 };
+
+Queue.prototype.enqueue = function(value) {
+  this[this.count] = value;
+  this.count++;
+};
+
+Queue.prototype.dequeue = function() {
+  if (this.count > 0) {
+    var dequeued = this[0];
+    delete this[0];
+    for (var i = 1; i <= this.count; i++) {
+      var temp = this[i];
+      this[i - 1] = temp;
+    }
+    this.count--;
+    return dequeued;
+  }
+};
+
+Queue.prototype.size = function() {
+  return this.count;
+};
+
+
 
 
