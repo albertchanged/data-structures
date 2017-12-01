570064c3d217f515e2a3962367db70a190df1a47
albertchanged <albertchanged@gmail.com>
2017-11-30 18:17:12 -0800

    Push current Tree code
    This code satisfies tests using the addChild function, but not
    contains.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,10 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
 
   return newTree;
 };
@@ -11,9 +12,38 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var newNode = new Tree(value);
+
+  if (Object.keys(this)) {
+    this[value] = newNode;
+  }
+  this.children.push(newNode);
 };
 
 treeMethods.contains = function(target) {
+  if (target in this) { 
+    return true; 
+  }
+
+  // var searchChildren = function(root) {
+  console.log('first' + this.children);
+  if (Object.keys(this) === target) {
+    console.log('second hello'); 
+    return true;
+  }
+
+  if (this.children) {
+    for (let i = 0; i < this.children.length; i++) {
+      console.log(this.children[i]);
+      return this.children[i].contains(target);
+    }
+  }
+
+  return false;
+  // };
+  
+  return searchChildren(this);
+  // return false;
 };
 
 
