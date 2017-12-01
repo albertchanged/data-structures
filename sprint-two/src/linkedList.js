051f83f2c4198766d0a64d7a5c879c801b5718bc
albertchanged <albertchanged@gmail.com>
2017-11-30 18:16:39 -0800

    Push working LinkedList code
    This program satisfies all LinkedList tests in the Mocha suite.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,31 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
-  };
+    var newNode = new Node(value);
+    if (!list.head) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
+  };   
 
   list.removeHead = function() {
+    if (list.head) {
+      var removed = list.head.value;
+      list.head = list.head.next;
+      return removed;
+    }
   };
 
   list.contains = function(target) {
+    var current = list.head;
+    while (current) {
+      if (current.value === target) { return true; }
+      current = current.next;
+    }    
+    return false;
   };
 
   return list;
