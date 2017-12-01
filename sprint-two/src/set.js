7c63d5eaf619472c459fa68df657680a8ad1ca54
albertchanged <albertchanged@gmail.com>
2017-11-30 21:11:45 -0800

    Push working Set code (pre-HashTable)
    This code satisfies all Set tests in Mocha suite.
    It does not utilize the HashTable yet.

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,34 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (!this.contains(item)) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  for (let i = 0; i < this._storage.length; i++) {
+    if (this._storage[i] === item) {
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  this._storage.splice(item);
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ *
+ * add: O(n) because we use the setPrototype.contains function
+ * contains: O(n) because we iterate over this._storage to find item
+ * remove: O(n) because splice is a built in Array math method
  */
