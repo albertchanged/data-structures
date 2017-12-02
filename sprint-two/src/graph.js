78b523ead1fb1c3b1f544fda3cb80afd409c8ec9
albertchanged <albertchanged@gmail.com>
2017-12-01 16:26:38 -0800

    Push current Graph code
    This passes all Node tests ONLY for Graph in Mocha suite.

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,38 +2,60 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  this.nodes = {};
+  this.edges = {};
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  // var newNode = new Graph(node);
+  this.nodes[node] = node;
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  return this.nodes[node] ? true : false;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  if (this.contains(node)) {
+    delete this.nodes[node];
+  }
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  for (var key in this.edges) {
+    if ((this.edges[key][0] === fromNode && this.edges[key][1] === toNode) || 
+        (this.edges[key][0] === toNode && this.edges[key][1] === fromNode)) {
+      return true;
+    }
+  }
+  return false;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this.edges[fromNode] = [fromNode, toNode];
+  this.edges[toNode] = [toNode, fromNode];
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  return this.hasEdge(fromNode, toNode) ? true : false;
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for (var key in this.nodes) {
+    cb(key);
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+
  */
 
 
