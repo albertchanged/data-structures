29feafaca6bd6ae4e5526712a57bb1ae29600124
albertchanged <albertchanged@gmail.com>
2017-12-01 16:26:01 -0800

    Push working HashTable code
    This program satisfies all HashTable tests in the Mocha suite.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,16 +5,34 @@ var HashTable = function() {
   this._storage = LimitedArray(this._limit);
 };
 
+// index is hash code
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (!this._storage.get(index)) {
+    this._storage.set(index, []);
+  }
+  this._storage.get(index).push([k, v]);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  
+  var result = undefined;
+  var innerArray = this._storage.get(index);
+
+  if (innerArray) {
+    for (let i = 0; i < innerArray.length; i++) {
+      if (innerArray[i][0] === k) {
+        result = innerArray[i][1];
+      }
+    }    
+  }
+  return result;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.set(index, undefined);
 };
 
 
